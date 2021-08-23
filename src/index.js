eb7af4853c7661ff4c2fe36037e5f9bcded647b6
Yuliya <yuliya.rostova@gmail.com>
2021-08-23 16:19:11 +0300

    feat: add solution to function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stackToCheck = [];
+  let strArray = str.split("");
+
+  let openBrackets = [];
+  let closeBrackets = [];
+
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    openBrackets.push(bracketsConfig[j][0]);
+    closeBrackets.push(bracketsConfig[j][1]);
+  }
+
+  for (let i = 0; i < strArray.length; i++) {
+    if (closeBrackets.includes(strArray[i])) {
+      if (stackToCheck.includes(openBrackets[closeBrackets.indexOf(strArray[i])])) {
+        //console.log('POP - ' + strArray[i]);
+        stackToCheck.pop();
+        continue;
+      }
+    }
+
+    if (openBrackets.includes(strArray[i])) {
+      //console.log('PUSH[' + i + '] - ' + strArray[i]);
+      stackToCheck.push(strArray[i]);
+      continue;
+    }
+    return false;
+  }
+
+  if (stackToCheck.length > 0) return false;
+  else return true;
 }
